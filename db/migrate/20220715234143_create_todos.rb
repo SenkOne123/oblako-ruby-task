fe807ca571b0e40e9d3b74f4b7e6c2ce23b05eb9
Nikita Mikryukov <senkone0123@gmail.com>
2022-07-16 04:49:10 +0500

    re-created models (project and todo), saving before migration

--- db/migrate/20220715234143_create_todos.rb
+++ db/migrate/20220715234143_create_todos.rb
@@ -2,7 +2,8 @@ class CreateTodos < ActiveRecord::Migration[7.0]
   def change
     create_table :todos do |t|
       t.string :text
-      t.boolean :isCompleted
+      t.boolean :isCompleted, :default => false
+      t.references :project, null: false, foreign_key: true
 
       t.timestamps
     end
