80a439f4f5398cb5716c818fa157b0d6752ef92d
Nikita Mikryukov <senkone0123@gmail.com>
2022-07-17 20:57:04 +0500

    debugging the problem with heroku db error

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,13 +1,5 @@
 class ProjectsController < ApplicationController
-=begin
-  def index
-    respond_to do |format|
-      format.html { render json: { message: "ok" }}
-    end
-  end
-=end
   def show
-    @projects = Project.all
-    render json: @projects
+    render json: Project.all
   end
 end
