87c0d8a324d8e88ef693bbcae415a895240b7d3b
Nikita Mikryukov <senkone0123@gmail.com>
2022-07-17 22:45:18 +0500

    added post query for project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,9 @@ class ProjectsController < ApplicationController
   def show
     render json: Project.all
   end
+  def create
+    Project.create(
+      title: params[:title]
+    )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
   get '/projects', to: 'projects#show'
+  post '/projects', to: 'projects#create'
   post '/todos', to: 'todos#create'
   patch '/projects/:project_id/todo/:todo_id', to: 'todos#update'
 end
