d5f8c017a05afd8d797af44eab10dc739aadb3e7
Nikita Mikryukov <senkone0123@gmail.com>
2022-07-18 23:39:58 +0500

    fixed the problem with POST queries

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,10 +1,15 @@
 class ProjectsController < ApplicationController
+
+  skip_before_action :verify_authenticity_token # Temporarily to check the POST and PATCH requests
+
   def show
     render json: Project.all.as_json(include: :todos)
   end
+
   def create
     Project.create(
       title: params[:title]
     )
   end
+
 end
